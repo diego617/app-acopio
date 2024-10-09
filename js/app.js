1c7fb3a25007ff675a6c1d72c697c044c5264c96
diego617 <diegotacuricabezas617@gmail.com>
2024-10-09 14:19:48 -0500

    Se agrego mas funcionalidad app.js

--- js/app.js
+++ js/app.js
@@ -4,23 +4,20 @@ function groupMonth(data){
   let months = {};
   let sumaK = 0;
   let sumaC = 0;
+  let total = "Total" 
   data.forEach(value => {
     const [year,month] = value.apfacturafecha.split('-');
     const date = new Date(year,month -1);
     const groupDate = nameMonth[date.getMonth()];
     sumaK += value.apfacturaapneto;
     sumaC += value.apfacturatotal;
-    let total = "Total" 
     if(!months[groupDate]){
-      months[groupDate] = {kilosnetos: 0, totalcompra:0},
-      months[total] = {kilosnetos:0,totalcompra:0}
+      months[groupDate] = {kilosnetos: 0, totalcompra:0}
     }
     months[groupDate].kilosnetos += value.apfacturaapneto;
     months[groupDate].totalcompra += value.apfacturatotal;
-    months[total].kilosnetos = sumaK;
-    months[total].totalcompra = sumaC;
   });
-  //console.log(months)
+  months[total] = {kilosnetos:sumaK,totalcompra:sumaC};
   listMonth(months);
 }
 const table = document.getElementById('table-content');
@@ -33,15 +30,21 @@ function createElementHtml(datos){
 
 function listMonth(values){
   
-  //createElementHtml(values)
-  for(const[key,value] of Object.entries(values).sort()){
-    let tds = value.kilosnetos;
-    createElementHtml(tds)
-    //console.log(tds);
+  //console.log(values)
+  for(const[key,value] of Object.entries(values)){
+    //console.log(value);
     const row = document.createElement('tr');
     const cell = document.createElement('td');
+    const cellKg = document.createElement('td');
+    const cellPro = document.createElement('td');
+    const cellCom = document.createElement('td');
     row.appendChild(cell).textContent = key;
+    row.appendChild(cellKg).textContent = value.kilosnetos.toLocaleString('en-US');
+    row.appendChild(cellPro).textContent = value.totalcompra / value.kilosnetos
+    
+    row.appendChild(cellCom).textContent = value.totalcompra.toLocaleString('en-US');
     table.appendChild(row);
+   //console.log(value.apfacturaapneto[1])
   }
 
 
