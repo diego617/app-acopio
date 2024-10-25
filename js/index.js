8dd04deac4792996a869e05a428e550b475033c5
diego617 <diegotacuricabezas617@gmail.com>
2024-10-25 18:09:33 -0500

    stilos

--- js/index.js
+++ js/index.js
@@ -1,9 +1,12 @@
 
-const acopio = document.querySelector('ul');
+const menu = document.getElementById('opcion');
+const home = document.getElementById('home');
+const acopio = document.getElementById('acopio');
 const main = document.getElementById('content');
 console.log(main)
 
-acopio.addEventListener('click',()=>{
+menu.addEventListener('click',(e)=>{
   main.innerHTML = '<h1>PÁGINA ACOPIO</h1>'
   console.log("pagina acopio")
+  console.log(e);
 });
