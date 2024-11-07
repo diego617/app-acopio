47d1a201b1392719e5670f0139af8809d4e9c275
diego617 <diegotacuricabezas617@gmail.com>
2024-11-06 19:00:24 -0500

    Se modifico el contenido

---
+++ js/home.js
@@ -0,0 +1,20 @@
+
+const tbody = document.querySelector('.table-content');
+const months = ['Ene','Feb','Mar','Abr','May','Jun','Jul','Ago','Set','Oct','Nov','Dic']
+function showTable(){
+  if(tbody){
+    tbody.innerHTML = '';
+    for(let row of months){
+      const tr = document.createElement('tr');
+      const td = document.createElement('td');
+      tr.appendChild(td).textContent = row;
+      console.log(row)
+      tbody.appendChild(tr);
+    }
+  }else{
+    console.error("No se encuentra el elemento tbody")
+  }
+}
+
+showTable()
+
--- js/index.js
+++ js/index.js
@@ -1,12 +1,52 @@
 
-const menu = document.getElementById('opcion');
-const home = document.getElementById('home');
-const acopio = document.getElementById('acopio');
-const main = document.getElementById('content');
-console.log(main)
+const links = document.querySelectorAll('#navigation a');
 
-menu.addEventListener('click',(e)=>{
-  main.innerHTML = '<h1>PÁGINA ACOPIO</h1>'
-  console.log("pagina acopio")
-  console.log(e);
+const linkMap ={
+  'acopio.html': 'js/home.js',
+  'contratos.html': 'js/contratos.js',
+}
+
+document.addEventListener('DOMContentLoaded',()=>{
+  links.forEach(link =>{
+    link.addEventListener('click',(event)=>{
+      event.preventDefault();
+      const url = link.getAttribute('href')
+      loadContent(url)
+    });
+  });
+
 });
+
+function loadContent(url) {
+  removePreviousScript()
+  fetch(url)
+      .then(response => {
+          if (!response.ok) {
+              throw new Error('Error en la respuesta de la red');
+          }
+          return response.text();
+      })
+      .then(data => {
+          document.getElementById('main-content').innerHTML = data;
+          console.log(data)
+          const scriptName = linkMap[url];
+            if (scriptName && !document.querySelector(`script[src="${scriptName}"]`)) {
+                const script = document.createElement('script');
+                script.src = scriptName;
+                script.id = 'dynamic-script';
+                script.onload = () => {
+                    console.log(`${scriptName} ha sido cargado y ejecutado.`);
+                };
+                document.body.appendChild(script);
+              }
+      })
+      .catch(error => console.error('Error al cargar el contenido:', error));
+}
+function removePreviousScript() {
+  const previousScript = document.getElementById('dynamic-script');
+  if (previousScript) {
+      previousScript.remove();
+  }
+}
+
+
