f5d5687c6852a9445ec6bce59cc60b394c20e59f
diego617 <diegotacuricabezas617@gmail.com>
2024-11-12 18:19:14 -0500

    nueva actualizacion

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,16 +1,20 @@
 import ConexionJson from "./ConexionJson.js";
 
-const month = ['Ene','Feb','Mar','Abr','May','Jun','Jul','Ago','Set','Oct','Nov','Dic'];
+const nameMonth = ['Ene','Feb','Mar','Abr','May','Jun','Jul','Ago','Set','Oct','Nov','Dic'];
 
-export function dashboard(data){
+export function dashboard(){
   const main = document.getElementById('main');
   const acopio = new ConexionJson();
   acopio.getData()
     .then(data =>{
       data.forEach(item =>{
+        const [year,month ] = item.apfacturafecha.split('-');
+        //console.log(item.apfacturafecha)
+        const date = new Date(year,month -1);
+        const groupDate = nameMonth[date.getMonth()];
         const span = document.createElement('span');
-        //main.appendChild(span).textContent = month;
-        console.log(item);
+        main.appendChild(span).textContent = groupDate;
+        console.log(groupDate);
       })
     }) 
 }
