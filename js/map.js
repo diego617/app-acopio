fc3da0d5c4f12e9f6322336f119d146456761be3
diego617 <diegotacuricabezas617@gmail.com>
2024-12-14 09:11:49 -0500

    pruebas

--- js/map.js
+++ js/map.js
@@ -3,4 +3,30 @@ const obj = {key:{id:"05dfx85",product:"Laptop"},
             key2:{id:'fdf884t6#$',product:"Impresora"}}
 let ar = Object.values(obj).map(index => index.product);
 
-console.log(ar);
+//console.log(ar);
+
+/*function maps(){
+  const mayor = Math.max(...array.map(item => {
+    /*if(item >= 100){
+      let ms = "El mumero mayo es: "+ item
+      return console.log(ms);
+      }else{
+        console.log("El numero es diferente.....!")
+    }
+    return item;
+    }))
+    console.log("El número mayor es:",mayor);
+    }*/
+   //maps()
+   
+const array = [10,85];
+function last(){
+  array.forEach((item,index)=>{
+    let res1 = item
+    console.log(item,index);
+    if(index){}
+  })
+}
+last()
+
+
